0331b9e7926294d3f865db6edd339674ea44a0e8
Dimitri-Habruk <111229468+Dimitri-Habruk@users.noreply.github.com>
2022-08-14 13:59:46 +0200

    Add files via upload

---
+++ send.php
@@ -0,0 +1,15 @@
+<?php
+/* Здесь проверяется существование переменных */
+if (isset($_POST['fb-1'])) {$phone = $_POST['name'];}
+if (isset($_POST['phone'])) {$name = $_POST['phone'];}
+ 
+/* Сюда впишите свою эл. почту */
+$myaddres  = "d.habruk@gmail.com"; // кому отправляем
+
+$sub='Заказ'; //сабж
+$email='Заказ обратного звонка'; // от кого
+$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+ 
+ini_set('short_open_tag', 'On');
+header('Refresh: 3; URL=vlavan.html');
+?>
